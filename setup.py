eca61d2115155e5d40b8bd299fa04b32c0d5d2aa
Szczurowsky <70973416+Szczurowsky@users.noreply.github.com>
2021-10-09 17:02:07 +0200

    Setup package for publish in pypi. (#2)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from pylogger import __version__
+
+setup(
+    name='py-logger',
+    url='https://github.com/Szczurowsky/py-loggerr',
+    author='Kamil Szczurowski',
+    author_email='kamil@szczurowsky.pl',
+    packages=['pylogger'],
+    version=__version__,
+    license='MPL-2.0',
+    description='Configurable and easy extendable python logger.',
+    long_description=open('README.txt').read(),
+)
